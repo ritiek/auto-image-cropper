3b514e39b006abf10296c80833cda467f79f8c02
Divyansh2512 <2512sharmadivyansh@gmail.com>
2020-08-16 02:10:34 +0530

    Python code refactor

--- python/autocrop.py
+++ python/autocrop.py
@@ -1,52 +1,81 @@
 #!/usr/bin/env python
 
 from PIL import Image
-from sys import argv
+import argparse
 
-if len(argv) == 3:
-	path = argv[1]
-	output = argv[2]
-else:
-	print("usage: " + argv[0] + " <input_image> <output_image>")
-	exit()
+def get_arguments():
+	parser = argparse.ArgumentParser()
 
-def getTopLeft(path):
-	im = Image.open(path)
-	for x in range(im.size[1]):
-		for y in range(im.size[0]):
-			color = im.getpixel((y, x))
-			if not color[0] == 255 and not color[1] == 255 and not color[2] == 255:
-				return x # top
+	parser.add_argument(
+		"-i",
+		"--input",
+		help="Path of Input Image"
+	)
+	parser.add_argument(
+		"-o",
+		"--output",
+		help="Path to Output Image"
+	)
 
-def getTopRight(path):
-	im = Image.open(path)
-	for x in range(im.size[0]):
-		for y in range(im.size[1]):
-			color = im.getpixel((x, y))
-			if not color[0] == 255 and not color[1] == 255 and not color[2] == 255:
-				return x # left
+	args = parser.parse_args()
+	if not args.input or not args.output:
+		parser.error("Please specify the path for image")
 
-def getLowerLeft(path):
-	im = Image.open(path)
-	for x in range(im.size[1]-1, 0, -1):
-		for y in range(im.size[0]-1, 0, -1):
-			color = im.getpixel((y, x))
-			if not color[0] == 255 and not color[1] == 255 and not color[2] == 255:
-				return (x + 1) # down
+	return args
 
-def getLowerRight(path):
-	im = Image.open(path)
-	for x in range(im.size[0]-1, 0, -1):
-		for y in range(im.size[1]-1, 0, -1):
-			color = im.getpixel((x, y))
-			if not color[0] == 255 and not color[1] == 255 and not color[2] == 255:
-				return (x + 1) # right
 
-b, a = getTopLeft(path), getTopRight(path)
-y, x = getLowerLeft(path), getLowerRight(path)
-strings = "(" + str(a) + ", " + str(b) + ", " + str(x) + ", " + str(y) + ")"
-print("Cropping area " + strings + " from " + path + " to " + output)
+class AutoCrop:
+	def __init__(self, img):
+		self.img = img
 
-im = Image.open(path)
-cropped = im.crop((a, b, x, y))
-cropped.save(output)
+	def get_top_left(self):
+		for x in range(self.img.size[1]):
+			for y in range(self.img.size[0]):
+				color = self.img.getpixel((y, x))
+				if color != (255, 255, 255):
+					return x # top
+
+	def get_top_right(self):
+		for x in range(self.img.size[0]):
+			for y in range(self.img.size[1]):
+				color = self.img.getpixel((x, y))
+				if color != (255, 255, 255):
+					return x # left
+
+	def get_lower_left(self):
+		for x in range(self.img.size[1]-1, 0, -1):
+			for y in range(self.img.size[0]-1, 0, -1):
+				color = self.img.getpixel((y, x))
+				if color != (255, 255, 255):
+					return (x + 1) # down
+
+	def get_lower_right(self):
+		for x in range(self.img.size[0]-1, 0, -1):
+			for y in range(self.img.size[1]-1, 0, -1):
+				color = self.img.getpixel((x, y))
+				if color != (255, 255, 255):
+					return (x + 1) # right
+
+	def new_image_coordinates(self):
+		a = self.get_top_right()
+		b = self.get_top_left() 
+		x = self.get_lower_right()
+		y = self.get_lower_left()
+
+		return (a, b, x, y)
+
+
+if __name__ == "__main__":
+	args = get_arguments()
+	path = args.input
+	output = args.output
+
+	img = Image.open(path)
+
+	img_crop = AutoCrop(img)
+	coordinates = img_crop.new_image_coordinates()
+
+	print("Cropping area " + str(coordinates) + " from " + path + " to " + output)
+
+	cropped = img.crop(coordinates)
+	cropped.save(output)
